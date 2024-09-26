67c3d10cc079927b4b718dfd481a3b6dec078cb4
Simon Boijer <boijersi@arcada.fi>
2024-09-26 11:15:39 +0300

    profiles fetching logic fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String fileName = "src/main/java/org/example/profiles.txt";
+
+        ArrayList<String> profiles = readProfilesFromFile(fileName);
+
+        printProfiles(profiles);
+    }
+
+    public static ArrayList<String> readProfilesFromFile(String fileName) {
+        ArrayList<String> profiles = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                profiles.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading profiles from file: " + fileName);
+        }
+
+        return profiles;
+    }
+
+    public static void printProfiles(ArrayList<String> profiles) {
+        String[] raderArray = profiles.toArray(new String[0]);
+
+        for (String rad : raderArray) {
+            System.out.println(rad);
+        }
     }
 }
